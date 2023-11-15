189074cdfce74fcee2d04322ce68cedd9855f3bb
Giorgia-N <nthabigiorgia19@gmail.com>
2023-11-15 10:30:10 -0800

    All to work

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num1 = 10;
+	int num2 = 20;
+	int sum = num1 + num2;
+
+	printf("The sum of %d and %d is %d\n", num1, num2, sum);
+	return (0);
+}
